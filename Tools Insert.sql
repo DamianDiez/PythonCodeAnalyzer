06bbc5b8009bc1933728d6d4989fa610eabbb067
Damian Diez <d.a.diez87@gmail.com>
2022-08-30 09:02:33 -0300

    Se agregar script insert tools

---
+++ Tools Insert.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO [python_code_analyzer_app_tool] ([name],[parameters]) VALUES ('Pylint','-a');
+INSERT INTO [python_code_analyzer_app_tool] ([name],[parameters]) VALUES ('Pysmell','-');
+INSERT INTO [python_code_analyzer_app_tool] ([name],[parameters]) VALUES ('PyRef','-');
+INSERT INTO [python_code_analyzer_app_tool] ([name],[parameters]) VALUES ('Vulture','-');
+INSERT INTO [python_code_analyzer_app_tool] ([name],[parameters]) VALUES ('Radon','-');
