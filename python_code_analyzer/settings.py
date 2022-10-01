a872b121394af7564876b3724e1f3719070a514b
Damian Diez <d.a.diez87@gmail.com>
2022-09-30 22:06:35 -0300

    mejoras en la apariencia de la aplicacion con bootstrap

--- python_code_analyzer/settings.py
+++ python_code_analyzer/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     # My apps
     'python_code_analyzer_app',
     'users',
+    # Third party apps.
+    'bootstrap4',
     # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
