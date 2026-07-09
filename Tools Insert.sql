49fe994c802a600816206833fb704e01a16f0ff8
Damian Diez <d.a.diez87@gmail.com>
2026-07-09 10:51:54 -0300

    Corrige Tools Insert.sql: elimina herramientas sin implementar y campo obsoleto
    - Elimina Pysmell y PyRef (nunca implementadas)
    - Elimina columna parameters (eliminada en migración 0103)
    - Agrega columna class_name para las 3 herramientas existentes
    - Agrega comentario con contexto de compatibilidad

--- Tools Insert.sql
+++ Tools Insert.sql
@@ -1,5 +1,7 @@
-﻿INSERT INTO [python_code_analyzer_app_tool] ([name],[parameters]) VALUES ('Pylint','-a');
-INSERT INTO [python_code_analyzer_app_tool] ([name],[parameters]) VALUES ('Pysmell','-');
-INSERT INTO [python_code_analyzer_app_tool] ([name],[parameters]) VALUES ('PyRef','-');
-INSERT INTO [python_code_analyzer_app_tool] ([name],[parameters]) VALUES ('Vulture','-');
-INSERT INTO [python_code_analyzer_app_tool] ([name],[parameters]) VALUES ('Radon','-');
+﻿-- Herramientas disponibles para análisis de código Python.
+-- Correr después de python manage.py migrate.
+-- Solo incluye herramientas con implementación en python_code_analyzer_app/app_models/Tool.py.
+-- Compatible con el modelo posterior a la migración 0103 (se eliminó el campo parameters).
+INSERT INTO [python_code_analyzer_app_tool] ([name],[class_name]) VALUES ('Pylint','Pylint_Tool');
+INSERT INTO [python_code_analyzer_app_tool] ([name],[class_name]) VALUES ('Vulture','Vulture_Tool');
+INSERT INTO [python_code_analyzer_app_tool] ([name],[class_name]) VALUES ('Radon','Radon_Tool');
